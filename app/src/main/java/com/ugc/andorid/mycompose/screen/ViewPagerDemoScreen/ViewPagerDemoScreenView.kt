198faede8ccd9d5f339b635ded91e5864e7eba9e
zhujunwen <zhujunwen@bytedance.com>
2022-03-21 11:55:36 +0800

    viewpager + tab

--- app/src/main/java/com/ugc/andorid/mycompose/screen/ViewPagerDemoScreen/ViewPagerDemoScreenView.kt
+++ app/src/main/java/com/ugc/andorid/mycompose/screen/ViewPagerDemoScreen/ViewPagerDemoScreenView.kt
@@ -5,23 +5,28 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import com.ugc.andorid.mycompose.screen.TabDemoScreen.BottomBar
 import com.ugc.andorid.mycompose.screen.TabDemoScreen.TabDemoScreenView
-
+import kotlinx.coroutines.launch
 
 
 @ExperimentalPagerApi
 @Composable
 fun ViewPagerDemoScreenView() {
     Column {
+        val pagerState = rememberPagerState()
         val viewModel: ViewPagerDemoScreenViewModel = viewModel()
-        HorizontalPager(count = 4) { pager ->  // implementation "com.google.accompanist:accompanist-pager:<version>"
+        HorizontalPager(count = 4, Modifier.weight(1f),
+        pagerState) { pager ->  // implementation "com.google.accompanist:accompanist-pager:<version>"
             when (pager) {
                 0 -> SportList()
                 1 -> Box(modifier = Modifier.fillMaxSize())
@@ -29,14 +34,21 @@ fun ViewPagerDemoScreenView() {
                 3 -> Box(modifier = Modifier.fillMaxSize())
             }
         }
-        BottomBar(viewModel.selectedTab) {
 
+        val scope = rememberCoroutineScope()
+        BottomBar(pagerState.currentPage) { page ->
+            // 点击标签页后，在协程里翻页
+            scope.launch {
+                pagerState.animateScrollToPage(page = page)
+            }
         }
     }
 }
 
 @Composable
 fun SportList() {
-    Box(modifier = Modifier.fillMaxSize().background(Color.Black))
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black))
 }
 
